a29264eaa7c44d140bf23a54151c9d409e1e2a08
陳寶桁 <>
2021-02-28 13:48:27 +0800

    fix git hib action issue

--- src/pages/blogPostTemplate.tsx
+++ src/pages/blogPostTemplate.tsx
@@ -18,7 +18,7 @@ const BlogPostTemplate = ({data}) => {
 export default BlogPostTemplate;
 
 export const pageQuery = graphql`
-  query($slug: String!) {
+  query($slug: String) {
     markdownRemark(frontmatter: { slug: { eq: $slug } }) {
       html
       frontmatter {
